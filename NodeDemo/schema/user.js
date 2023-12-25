61e6d29dfa6dab60f85b99ac930d12da379038ca
PhanHongYenQuynh <luisywang1411@gmail.com>
2023-12-25 10:55:51 +0700

    Update CheckLogin

--- NodeDemo/schema/user.js
+++ NodeDemo/schema/user.js
@@ -1,15 +1,13 @@
 var mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const configs = require("../helper/configs");
 
 const schema = new mongoose.Schema({
   email: String,
   userName: String,
   password: String,
-  role: {
-    type: String,
-    enum: ["user", "admin"], // Define the possible roles
-    default: "user", // Set a default role if not provided
-  },
+  role: String,
 });
 
 schema.pre("save", function () {
@@ -18,5 +16,27 @@ schema.pre("save", function () {
   //bug sinh ra khi change password
 });
 
-module.exports = mongoose.model('user', schema);;
+schema.methods.getJWT = function () {
+  var token = jwt.sign({ id: this._id }, configs.SECRET_KEY, {
+    expiresIn: configs.EXP,
+  });
+  return token;
+};
+schema.statics.checkLogin = async function (userName, password) {
+  if (!userName || !password) {
+    return { err: "Hay nhap day du username va password" };
+  }
+  var user = await this.findOne({ userName: userName });
+  if (!user) {
+    return { err: "userName khong ton tai" };
+  }
+  var result = bcrypt.compareSync(password, user.password);
+  if (!result) {
+    return { err: "password sai" };
+  }
+  console.log(user);
+  return user;
+};
+//JWT
 
+module.exports = mongoose.model("user", schema);
