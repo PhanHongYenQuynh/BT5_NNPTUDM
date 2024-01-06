a371286294596493fa32ab515cf1a7db16ec7696
PhanHongYenQuynh <luisywang1411@gmail.com>
2024-01-06 11:10:19 +0700

    Update Mail again

--- Nodejs/NodeDemo/helper/configs.js
+++ Nodejs/NodeDemo/helper/configs.js
@@ -1,9 +1,9 @@
-module.exports= {
-    SECRET_KEY:"heheheehe",
-    EXP: '20d',
-    cookieExp: 2,
-    MAIL_HOST:'sandbox.smtp.mailtrap.io',
-    MAIL_PORT:2525,
-    MAIL_USER: 'ed02b175797329',
-    MAIL_PASS:'4b1aeaafd99196',
-}
+module.exports = {
+  SECRET_KEY: "heheheehe",
+  EXP: "20d",
+  cookieExp: 2,
+  MAIL_HOST: "sandbox.smtp.mailtrap.io",
+  MAIL_PORT: 2525,
+  MAIL_USER: "2806a843c98f80",
+  MAIL_PASS: "123b59fdcd32b5",
+};
